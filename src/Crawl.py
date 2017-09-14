0c978e5bcdeaba912e5e0d10577b924191d485d0
Eric-LiuXin <liuxin_coding@sina.com>
2017-09-15 00:10:04 +0800

    Add exception handling

--- src/Crawl.py
+++ src/Crawl.py
@@ -12,7 +12,7 @@ QueryTime = {8}\
 '''
 class Crawl:
     def __init__(self, start_urls):
-        self.date_list = [str(datetime.date.today() + datetime.timedelta(days=key)) for key in range(0, 30)]
+        self.date_list = [str(datetime.date.today() + datetime.timedelta(days=key)) for key in range(1, 30)]
         self.price_pannel_list = list()
         self.driver = webdriver.PhantomJS()
         self.driver.set_page_load_timeout(10) 
@@ -24,48 +24,56 @@ class Crawl:
     def GotoPage(self, date):
         data_js = "document.getElementById(\"DDate1\").value=\"{0}\"".format(date)
         self.driver.execute_script(data_js)
+        time.sleep(1)
         self.driver.find_element_by_id('btnReSearch').click()
         for i in range(10):
             self.driver.execute_script(self.scroll_js)
             time.sleep(2)
         
     def DateParse(self, date):
-        self.GotoPage(date)
-        elements = self.driver.find_elements_by_class_name("J_header_row")
-        print (len(elements))
-        for element in elements:
-            try:
-                price_pannel = PricePannel()
-                price_pannel.air_route = self.depart_city + "-" + self.arrive_city
-                flight_company_element = element.find_element_by_class_name("logo")
-                arr = flight_company_element.text.split('\n')
-                price_pannel.company = arr[0]
-                price_pannel.flight = arr[1]
-                price_pannel.aircraft_type = arr[2]
-                                
-                right_element = element.find_element_by_class_name("right")
-                arr = right_element.text.split('\n')
-                price_pannel.departure_time = date
-                price_pannel.takeoff_airport = arr[1]
-                    
-                left_element = element.find_element_by_class_name("left")
-                arr = left_element.text.split('\n')
-                price_pannel.arrival_airport = arr[1]
+        try:
+            self.GotoPage(date)
+            elements = self.driver.find_elements_by_class_name("J_header_row")
+            for element in elements:
+                try:
+                    price_pannel = PricePannel()
+                    price_pannel.air_route = self.depart_city + "-" + self.arrive_city
+                    flight_company_element = element.find_element_by_class_name("logo")
+                    arr = flight_company_element.text.split('\n')
+                    price_pannel.company = arr[0]
+                    price_pannel.flight = arr[1]
+                    price_pannel.aircraft_type = arr[2]
+                                    
+                    right_element = element.find_element_by_class_name("right")
+                    arr = right_element.text.split('\n')
+                    price_pannel.departure_time = date
+                    price_pannel.takeoff_airport = arr[1]
                         
-                price_element = element.find_element_by_class_name("price")
-                price_pannel.price = re.sub(r'\D', "", price_element.text)
-                
-                self.price_pannel_list.append(price_pannel)
-            except:
-                continue
+                    left_element = element.find_element_by_class_name("left")
+                    arr = left_element.text.split('\n')
+                    price_pannel.arrival_airport = arr[1]
+                            
+                    price_element = element.find_element_by_class_name("price")
+                    price_pannel.price = re.sub(r'\D', "", price_element.text)
+                    
+                    self.price_pannel_list.append(price_pannel)
+                except:
+                    continue
+        except:
+            pass
     
     def Run(self):
         for url in self.start_urls:
+            print (url)
             self.driver.get(url)
-            self.depart_city = self.driver.execute_script(self.depart_city_js)
-            self.arrive_city = self.driver.execute_script(self.arrive_city_js)
+            while not self.depart_city or not self.arrive_city:
+                self.depart_city = self.driver.execute_script(self.depart_city_js)
+                self.arrive_city = self.driver.execute_script(self.arrive_city_js)
+                time.sleep(1)
             for date in self.date_list:
                 self.DateParse(date)
+            
+            self.depart_city, self.arrive_city = None, None
         
     def Finish(self):
         self.driver.quit()
